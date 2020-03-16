47967b2342f8bc7980bc34309146ff8e7c6280e7
Pavneet-Sing <dev.pavneet@gmail.com>
2020-03-16 22:10:52 +0530

    directory change fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,15 @@ apt-get update
 apt install wget curl git -y
 export LANG=en_US.UTF-8
 REPO=$( echo "$URL" | sed -e "s/\([^/]*\/\/\)\?\(www\.\)\?\([^.]*\)\..*/\3/")
-wget -mpk $URL
+wget -mpkP ./LOCAL $URL
+cd LOCAL
+cd $(ls)
 curl \
 	-H "Authorization: token $_GITHUB_TOKEN" https://api.github.com/user/repos \
 	-d '{"name":"'$REPO'-website","private":false}'
-cd $URL
 git init
 git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_EMAIL"
 git add .
-git commit -m "init website repo"
+git commit -m "initialize website repo"
 git push -f https://$_GITHUB_TOKEN:x-oauth-basic@github.com/$USER_NAME/$REPO-website.git master
