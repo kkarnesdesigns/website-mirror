acc1f4b32489985a28f666efaac017149c5f2e57
kkarnesdesigns <40835120+kkarnesdesigns@users.noreply.github.com>
2022-09-21 22:58:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,16 @@ apt-get update
 apt install wget curl git -y
 export LANG=en_US.UTF-8
 REPO=$( echo "$URL" | sed -e "s/\([^/]*\/\/\)\?\(www\.\)\?\([^.]*\)\..*/\3/")
-wget -m -p -E -k ./LOCAL $URL
+wget -m ./LOCAL $URL
 cd LOCAL
 cd $(ls)
 curl \
 	-H "Authorization: token $_GITHUB_TOKEN" https://api.github.com/user/repos \
 	-d '{"getac":"'$REPO'-website","private":false}'
 git init
-git config --global --add safe.directory "*"
 git config --global user.name "kkarnesdesigns"
 git config --global user.email "kyle@kkarnesdesigns.com"
+git config --global --add safe.directory "*"
 git add .
-git commit -m "initial commit"
+git commit -m "initialize website repo"
 git push -f https://$_GITHUB_TOKEN:x-oauth-basic@github.com/kkarnesdesigns/$REPO-website.git master
